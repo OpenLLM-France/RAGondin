ca247d302d3cca8161adbaec135ce0d95d766669
Ahmath-GADJI <ahmathgadji@gmail.com>
2024-10-18 13:57:45 +0000

    update apps

--- app/chainlit_app.py
+++ app/chainlit_app.py
@@ -5,29 +5,35 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
 from src.components import RagPipeline, Config
 from loguru import logger
 
-config = Config("../config.ini")
+APP_DIR = Path(__file__).parent.absolute()
+UPLOAD_DIR = APP_DIR / "upload_dir"
+os.makedirs(UPLOAD_DIR, exist_ok=True)
+
+config = Config(APP_DIR.parent / "config.ini")
 ragPipe = RagPipeline(config=config)
 
+
+# https://github.com/Cinnamon/kotaemon/blob/main/libs/ktem/ktem/reasoning/prompt_optimization/suggest_followup_chat.py
 
 @cl.set_starters
 async def set_starters():
     return [
         cl.Starter(
-            label="IA Générative",
-            message="Quels sont les bénéfices de l’IA générative?",
+            label="OpenLLM France",
+            message="Quel est l'objectif d'OpenLLM France et quels sont les enjeux?",
             icon="./public/idea.svg",
     ),
 
     cl.Starter(
-            label="CLOUD ACT vs RGPD",
-            message="Comment le CLOUD ACT se conforme au RGPD?",
-            icon="./public/danger-triangle.svg",
+            label="Les logiciels développés de Linagora",
+            message="Quels sont produits logiciels développés par Linagora?",
+            icon="./public/labor-man-labor.svg",
             ),
 
     cl.Starter(
-            label="Digital labor",
-            message='Définition et effets de la notion de "Digital Labor".',
-            icon="./public/labor-man-labor.svg",
+            label="Présentation de Linagora",
+            message="Fais moi une présentation de Linagora.",
+            icon= "./public/danger-triangle.svg",
             ),
     ]
 
@@ -38,20 +44,26 @@ async def on_chat_start():
     logger.info("Chat history flushed")
      
 
-
 @cl.on_message
 async def main(message: cl.Message):
     question = message.content
 
     async with cl.Step(name="Searching for relevant documents...") as step:
-        stream, _, sources = ragPipe.run(question)
+        stream, _, sources, docs = ragPipe.run(question)
     await step.remove()
 
 
-    elements = [
-        cl.Pdf(name=ref, path=s, page=p, display="side", size="medium") 
-        for ref, s, p in sources
-    ]
+    # elements = [
+    #     cl.Pdf(name=ref, path=s, page=p, display="side", size="medium") 
+    #     for ref, s, p in sources
+    # ]
+
+    elements = []
+    source_names = []
+    for n, doc in enumerate(docs, start=1):
+        source_names.append(f'[doc_{n}]')
+        elements.append(cl.Text(content=doc.page_content, name=f'[doc_{n}]', display='side'))
+    
     msg = cl.Message(content="", elements=elements)
     await msg.send()
     
@@ -63,4 +75,5 @@ async def main(message: cl.Message):
     if ragPipe.rag_mode == "ChatBotRag":
             ragPipe.update_history(question, answer_txt)
 
+    await msg.stream_token("\n\nRetreived Docs: " + ', '.join(source_names))
     await msg.send()
