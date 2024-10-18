9050aba0f25ad3fb84a6da79417d5c2a7198faac
Ahmath-GADJI <ahmathgadji@gmail.com>
2024-10-18 14:02:36 +0000

    singleton metaclass added

--- src/components/utils.py
+++ src/components/utils.py
@@ -1,6 +1,19 @@
 from pathlib import Path
 from langchain_core.documents.base import Document
 
+class SingletonMeta(type):
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            # print("1st creation")
+            instance = super().__call__(*args, **kwargs)
+            cls._instances[cls] = instance
+        # else:
+        #     print("Same one")
+        return cls._instances[cls]
+    
+
 def load_sys_template(file_path: Path) -> tuple[str, str]:
     with open(file_path, mode="r") as f:
         sys_msg = f.read()
@@ -15,7 +28,7 @@ def format_context(docs: list[Document]) -> str:
     for i, doc in enumerate(docs, start=1):
         doc_id = f"[doc_{i}]"
         source = doc.metadata["source"]
-        page = doc.metadata["page"] + 1
+        page = doc.metadata["page"]
         
         context += f"{doc_id}\n"
         context += f"Content: {doc.page_content.strip()}\n"
