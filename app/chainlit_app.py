2e2d5321e5795475dd4b178f2115613bd757682f
Ahmath-GADJI <ahmathgadji@gmail.com>
2024-11-04 09:42:31 +0000

    MISC

--- app/chainlit_app.py
+++ app/chainlit_app.py
@@ -1,82 +1,86 @@
 from pathlib import Path
 import chainlit as cl
-import sys, os
+import sys, os, torch
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
-from src.components import RagPipeline, Config
+from src.components import RagPipeline, Config, AudioTranscriber
 from loguru import logger
+from io import BytesIO
 
-APP_DIR = Path(__file__).parent.absolute()
+APP_DIR = Path(__file__).parent.absolute() # Path.cwd().parent.absolute()
 UPLOAD_DIR = APP_DIR / "upload_dir"
 os.makedirs(UPLOAD_DIR, exist_ok=True)
 
 config = Config(APP_DIR.parent / "config.ini")
-ragPipe = RagPipeline(config=config)
-
+ragPipe = RagPipeline(config=config, device="cpu")
 
 # https://github.com/Cinnamon/kotaemon/blob/main/libs/ktem/ktem/reasoning/prompt_optimization/suggest_followup_chat.py
+
 
 @cl.set_starters
 async def set_starters():
     return [
         cl.Starter(
             label="OpenLLM France",
-            message="Quel est l'objectif d'OpenLLM France et les enjeux de ce projet?",
-            icon="./public/idea.svg",
+            message="Objectif d'OpenLLM France et les enjeux de ces projet?",
+            icon="/public/idea.svg",
         ),
 
         cl.Starter(
                 label="Produits de Linagora",
-                message="Parle moi du Pack Twake et de LinTo?",
-                icon="./public/labor-man-labor.svg",
-                ),
+                message="Parle moi du Pack Twake et du produit LinTo?",
+                icon="/public/labor-man-labor.svg",
+        ),
 
         cl.Starter(
                 label="Présentation de Linagora",
                 message="Fais moi une présentation de Linagora.",
-                icon= "./public/danger-triangle.svg",
-                )
+                icon= "/public/danger-triangle.svg",
+        )
     ]
-
-
-@cl.on_chat_start
-async def on_chat_start():
-    ragPipe._chat_history.clear()
-    logger.info("Chat history flushed")
      
 
 def format_elements(sources, only_txt=True):
     elements = []
     source_names = []
     for doc in sources:
-        source_names.append(doc['doc_id'])
-
         if only_txt:
             elem = cl.Text(content=doc["content"], name=doc['doc_id'], display='side')
+            s = f"{doc['doc_id']}: {Path(doc["source"]).name}"
         else:
             source = Path(doc["source"])
             match source.suffix:
                 case '.pdf':
-                    elem = cl.Pdf(name=doc['doc_id'], path=doc["source"], page=doc["page"], display="side")
-
+                    elem = cl.Pdf(name=doc['doc_id'], path=doc["source"], page=doc["page"], display='side')
+                    s = f"{doc['doc_id']}: {Path(doc["source"]).name} (p. {doc["page"]})"
                 case '.mp4':
                     elem = cl.Video(name=doc['doc_id'], path=doc["source"], display='side')
+                    s = f"{doc['doc_id']}: {Path(doc["source"]).name}"
                 
                 case '.mp3':
                     elem = cl.Audio(name=doc['doc_id'], path=doc["source"], display='side')
+                    s = f"{doc['doc_id']}: {Path(doc["source"]).name}"
                 
                 case _:
                     elem = cl.Text(content=doc["content"], name=doc['doc_id'], display='side') # TODO Maybe HTML (convert the File first)
+                    s = f"{doc['doc_id']}: {Path(doc["source"]).name}"
 
-        elements.append(elem)                
+        elements.append(elem) 
+        source_names.append(s)               
     return elements, source_names
-     
+
+@cl.on_chat_start
+async def on_chat_start():
+    ragPipe._chat_history.clear()
+    logger.info("Chat history flushed")
+
+
     
 @cl.on_message
-async def main(message: cl.Message):
+async def on_message(message: cl.Message):
     question = message.content
 
     async with cl.Step(name="Searching for relevant documents...") as step:
-        stream, _, sources = ragPipe.run(question)
+        stream, _, sources = await ragPipe.run(question)
     await step.remove()
 
     elements, source_names = format_elements(sources, only_txt=False)
@@ -84,6 +88,7 @@ async def main(message: cl.Message):
     await msg.send()
     
     answer_txt = ""
+    
     async for token in stream:
         await msg.stream_token(token.content)
         answer_txt += token.content
@@ -91,13 +96,55 @@ async def main(message: cl.Message):
     if ragPipe.rag_mode == "ChatBotRag":
             ragPipe.update_history(question, answer_txt)
 
-    await msg.stream_token("\n\nRetreived Docs: " + ', '.join(source_names))
+    await msg.stream_token( '\n\n' + '-'*50 + "\n\nRetreived Docs: \n" + '\n'.join(source_names))
     await msg.send()
+
+
+# from pydub import AudioSegment
+# import whisperx
+# from chainlit.element import ElementBased
+# device = 'cuda' if torch.cuda.is_available() else 'cpu'
+# audio_transcriber = AudioTranscriber(device=device).model
+
+
+# @cl.on_audio_chunk
+# async def on_audio_chunk(chunk: cl.AudioChunk):
+#     if chunk.isStart:
+#         buffer = BytesIO()
+#         buffer.name = f"input_audio.{chunk.mimeType.split('/')[1]}"
+#         cl.user_session.set("audio_buffer", buffer)
+#         cl.user_session.set("audio_mime_type", chunk.mimeType)
+
+#     # Write the chunks to a buffer
+#     cl.user_session.get("audio_buffer").write(chunk.data)
+
+# @cl.on_audio_end
+# async def on_audio_end(elements: list[ElementBased]):
+#     audio_buffer: BytesIO = cl.user_session.get("audio_buffer")
+#     # audio_mime_type: str = cl.user_session.get("audio_mime_type")
+#     audio_buffer.seek(0)  # Move the file pointer to the beginning
+
+#     try:
+#         sound = AudioSegment.from_file(audio_buffer)
+#         sound.export(
+#             "output.wav", format="wav", 
+#             parameters=["-ar", "16000", "-ac", "1", "-ab", "32k"]
+#         )
+#         trans_res= audio_transcriber.transcribe(
+#             audio=whisperx.load_audio('output.wav'), batch_size=8
+#         )
+#         transcription = ' '.join(s['text'] for s in trans_res["segments"])
+
+#         await cl.Message(content=f"transcription: {transcription}").send()
+
+#         await on_message(cl.Message(content=transcription))
+    
+#     except Exception as e:
+#         await cl.Message(content=f"Error processing audio: {str(e)}").send()
 
 
 if __name__ == "__main__":
     import sys
     from chainlit.cli import run_chainlit
-
     sys.argv.extend(["-w", "--no-cache"])
     run_chainlit(__file__)
