f4c3216a2d0cd6fc09e370d3655061a034e83f8b
MChrys <chrys.beltran@outlook.fr>
2024-11-07 12:54:59 +0000

    ssl https working

--- app/chainlit_app.py
+++ app/chainlit_app.py
@@ -2,7 +2,7 @@ from pathlib import Path
 import chainlit as cl
 import sys, os, torch, yaml
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
-from src.components import RagPipeline, Config #, AudioTranscriber
+from src.components import RagPipeline, Config , AudioTranscriber
 from loguru import logger
 from io import BytesIO
 
@@ -92,47 +92,47 @@ async def on_message(message: cl.Message):
 
 
 
-# from pydub import AudioSegment
-# import whisperx
-# from chainlit.element import ElementBased
-# device = 'cuda' if torch.cuda.is_available() else 'cpu'
-# audio_transcriber = AudioTranscriber(device=device).model
+from pydub import AudioSegment
+import whisperx
+from chainlit.element import ElementBased
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+audio_transcriber = AudioTranscriber(device=device).model
 
 
-# @cl.on_audio_chunk
-# async def on_audio_chunk(chunk: cl.AudioChunk):
-#     if chunk.isStart:
-#         buffer = BytesIO()
-#         buffer.name = f"input_audio.{chunk.mimeType.split('/')[1]}"
-#         cl.user_session.set("audio_buffer", buffer)
-#         cl.user_session.set("audio_mime_type", chunk.mimeType)
+@cl.on_audio_chunk
+async def on_audio_chunk(chunk: cl.AudioChunk):
+ if chunk.isStart:
+     buffer = BytesIO()
+     buffer.name = f"input_audio.{chunk.mimeType.split('/')[1]}"
+     cl.user_session.set("audio_buffer", buffer)
+     cl.user_session.set("audio_mime_type", chunk.mimeType)
 
-#     # Write the chunks to a buffer
-#     cl.user_session.get("audio_buffer").write(chunk.data)
+ # Write the chunks to a buffer
+ cl.user_session.get("audio_buffer").write(chunk.data)
 
-# @cl.on_audio_end
-# async def on_audio_end(elements: list[ElementBased]):
-#     audio_buffer: BytesIO = cl.user_session.get("audio_buffer")
-#     # audio_mime_type: str = cl.user_session.get("audio_mime_type")
-#     audio_buffer.seek(0)  # Move the file pointer to the beginning
+@cl.on_audio_end
+async def on_audio_end(elements: list[ElementBased]):
+ audio_buffer: BytesIO = cl.user_session.get("audio_buffer")
+ # audio_mime_type: str = cl.user_session.get("audio_mime_type")
+ audio_buffer.seek(0)  # Move the file pointer to the beginning
 
-#     try:
-#         sound = AudioSegment.from_file(audio_buffer)
-#         sound.export(
-#             "output.wav", format="wav", 
-#             parameters=["-ar", "16000", "-ac", "1", "-ab", "32k"]
-#         )
-#         trans_res= audio_transcriber.transcribe(
-#             audio=whisperx.load_audio('output.wav'), batch_size=8
-#         )
-#         transcription = ' '.join(s['text'] for s in trans_res["segments"])
+ try:
+     sound = AudioSegment.from_file(audio_buffer)
+     sound.export(
+         "output.wav", format="wav", 
+         parameters=["-ar", "16000", "-ac", "1", "-ab", "32k"]
+     )
+     trans_res= audio_transcriber.transcribe(
+         audio=whisperx.load_audio('output.wav'), batch_size=8
+     )
+     transcription = ' '.join(s['text'] for s in trans_res["segments"])
 
-#         await cl.Message(content=f"transcription: {transcription}").send()
+     await cl.Message(content=f"transcription: {transcription}").send()
 
-#         await on_message(cl.Message(content=transcription))
-    
-#     except Exception as e:
-#         await cl.Message(content=f"Error processing audio: {str(e)}").send()
+     await on_message(cl.Message(content=transcription))
+
+ except Exception as e:
+     await cl.Message(content=f"Error processing audio: {str(e)}").send()
 
 
 # chainlit run chainlit_app.py --host 0.0.0.0 --port 8000 --root-path /chainlit
