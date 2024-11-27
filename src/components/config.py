568022e294f2bdf83895bb0438743734bcfb5438
Ahmath-GADJI <ahmathgadji27@gmail.com>
2024-11-27 14:17:31 +0000

    Hydra config and overrides option added

--- src/components/config.py
+++ src/components/config.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv, find_dotenv
 from omegaconf import OmegaConf
+from pathlib import Path
 from hydra import initialize, compose
 
 def load_config(config_path="../../.hydra_config", overrides=None)-> OmegaConf:
@@ -9,6 +10,7 @@ def load_config(config_path="../../.hydra_config", overrides=None)-> OmegaConf:
         
     with initialize(config_path=config_path, job_name="config_loader"):
         config = compose(config_name="config", overrides=overrides)
+        config.paths.root_dir = Path(config.paths.root_dir).absolute()
         return config
 
 # # Example usage
