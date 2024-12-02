ce6e9f1158f43c5b6bf7b5877730b5b4bc4f3b90
Ahmath-GADJI <ahmathgadji27@gmail.com>
2024-12-02 21:57:23 +0000

    update fastapi, env and main.py

--- fast_api.py
+++ fast_api.py
@@ -1,7 +1,8 @@
 import asyncio
 from typing import Annotated
-from fastapi import FastAPI, File, UploadFile, HTTPException, status
+from fastapi import FastAPI, File, UploadFile, HTTPException, status, Request, Depends
 from enum import Enum
+import json
 # from chainlit.utils import mount_chainlit
 import sys, os
 from pydantic import BaseModel
@@ -23,13 +24,10 @@ os.makedirs(UPLOAD_DIR, exist_ok=True)
 
 config = load_config()
 
-# config.vectordb["host"] = "172.17.0.2"
-
 print("config.vectordb['host']", config.vectordb["host"])
 print("config.vectordb['port']", config.vectordb["port"])
 
 ragPipe = RagPipeline(config=config)
-
 
 class Tags(Enum):
     VDB = "VectorDB operations"
@@ -49,30 +47,49 @@ app = FastAPI()
 app.mount('/static', StaticFiles(directory=DATA_DIR.absolute()), name='static')
 
 
+def static_base_url_dependency(request: Request) -> str:
+    return f"{request.url.scheme}://{request.client.host}:{request.url.port}/static"
+
+
+def source2url(s: dict, static_base_url: str):
+    s['url'] = f"{static_base_url}/{s['sub_url_path']}"
+    s.pop("source")
+    s.pop('sub_url_path')
+    return s
+
+
 @app.post("/generate/",
           summary="Given a question, this endpoint allows to generate an answer grounded on the documents in the VectorDB",
           tags=[Tags.LLM]
           )
-async def get_answer(new_user_input: str, chat_history: list[ChatMsg]=None):
+async def get_answer(
+    new_user_input: str, chat_history: list[ChatMsg]=None,
+    static_base_url: str = Depends(static_base_url_dependency)
+    ):
 
     msgs: list[HumanMessage | AIMessage] = None
     if chat_history:
         msgs = [mapping[chat_msg.role](content=chat_msg.content) for chat_msg in chat_history]
     answer_stream, context, sources = await ragPipe.run(question=new_user_input, chat_history=msgs)
+    
+    sources = list(map(lambda x: source2url(x, static_base_url), sources))
+    src_json = json.dumps(sources)
 
     async def send_chunk():
-        async for chunk in answer_stream:
-            yield chunk.content
+        async for token in answer_stream:
+            yield token.content
           
-    return StreamingResponse(send_chunk(), media_type="text/event-stream")
+    return StreamingResponse(
+        send_chunk(), 
+        media_type="text/event-stream",
+        headers={"X-Metadata-Sources": src_json},
+    )
 
 
 @app.get("/hello/")
 async def hello():
     return "hello"
 
-
-# mount_chainlit(app=app, target="./chainlit_app.py", path="/chainlit")
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000) # 8083
