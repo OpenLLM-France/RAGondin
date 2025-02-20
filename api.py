b4a85166a59230fd15d4218df382a8c070145eb8
Mahamadoulng <mkeita@linagora.com>
2025-02-20 11:12:22 +0100

    renaming chainlit_app.py chainlit_fastapi.py

---
+++ api.py
@@ -0,0 +1,159 @@
+import asyncio
+from typing import Annotated
+from fastapi import FastAPI, File, UploadFile, HTTPException, status, Request, Depends
+from enum import Enum
+import json
+# from chainlit.utils import mount_chainlit
+import sys, os
+from pydantic import BaseModel
+from langchain_core.messages import AIMessage, HumanMessage
+from fastapi.responses import StreamingResponse
+from contextlib import asynccontextmanager
+from fastapi.staticfiles import StaticFiles
+import uvicorn
+from typing import Literal
+from pathlib import Path
+from src.components import RagPipeline, load_config, Indexer
+from loguru import logger
+
+APP_DIR = Path.cwd()
+DATA_DIR = APP_DIR / 'data'
+# Directory to store uploaded PDFs
+UPLOAD_DIR = APP_DIR / 'data' / "upload_dir"
+os.makedirs(UPLOAD_DIR, exist_ok=True)
+
+config = load_config()
+
+print("config.vectordb['host']", config.vectordb["host"])
+print("config.vectordb['port']", config.vectordb["port"])
+
+indexer = Indexer(config, logger)
+ragPipe = RagPipeline(config=config, vectordb=indexer.vectordb, logger=logger)
+
+class Tags(Enum):
+    VDB = "VectorDB operations"
+    LLM = "LLM Calls"
+
+class ChatMsg(BaseModel):
+    role: Literal["user", "assistant"]
+    content: str
+
+mapping = {
+    "user": HumanMessage, 
+    "assistant": AIMessage
+}
+
+app = FastAPI()
+app.mount('/static', StaticFiles(directory=DATA_DIR.absolute(), check_dir=True), name='static')
+
+
+def static_base_url_dependency(request: Request) -> str:
+    return f"{request.url.scheme}://{request.client.host}:{request.url.port}/static"
+
+
+def source2url(s: dict, static_base_url: str):
+    s['url'] = f"{static_base_url}/{s['sub_url_path']}"
+    s.pop("source")
+    s.pop('sub_url_path')
+    return s
+
+
+@app.get("/collections/",
+          summary="Get existant collections",
+          tags=[Tags.VDB]
+          )
+async def get_collections() -> list[str]:
+    return await indexer.vectordb.get_collections()
+
+
+@app.post("/generate/",
+          summary="Given a question, this endpoint allows to generate an answer grounded on the documents in the VectorDB",
+          tags=[Tags.LLM]
+          )
+async def get_answer(
+    new_user_input: str, chat_history: list[ChatMsg]=None,
+    static_base_url: str = Depends(static_base_url_dependency)
+    ):
+
+    msgs: list[HumanMessage | AIMessage] = None
+    if chat_history:
+        msgs = [mapping[chat_msg.role](content=chat_msg.content) for chat_msg in chat_history]
+    answer_stream, context, sources = await ragPipe.run(question=new_user_input, chat_history=msgs)
+    
+    sources = list(map(lambda x: source2url(x, static_base_url), sources))
+    src_json = json.dumps(sources)
+
+    async def send_chunk():
+        async for token in answer_stream:
+            yield token.content
+          
+    return StreamingResponse(
+        send_chunk(), 
+        media_type="text/event-stream",
+        headers={"X-Metadata-Sources": src_json},
+    )
+
+
+@app.get("/hello/", summary="Toy endpoint to check that the api is up")
+async def hello():
+    return "hello"
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8082) # 8083
+
+
+# uvicorn api:app --reload --port 8082
+
+# @asynccontextmanager
+# async def lifespan(app: FastAPI):
+#     # await ragPipe.indexer.add_files2vdb(UPLOAD_DIR)
+#     yield
+#     # Clean up the db
+#     ragPipe.indexer.connector.client.delete_collection(
+#         collection_name=config.vectordb["collection_name"]
+#     )
+
+# app = FastAPI(
+#     # lifespan=lifespan
+# )
+
+
+
+# async def process_data(file: UploadFile):
+#     mime_type = file.content_type
+#     if mime_type not in ['application/pdf']:
+#         raise HTTPException(
+#             status_code=status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
+#             detail=f"Unsupported file type: {mime_type}"
+#         )
+#     try:
+#         file_path = UPLOAD_DIR / file.filename
+#         with open(file_path, "wb") as f:
+#             f.write(await file.read())
+            
+#         # await ragPipe.indexer.add_file2vdb(file_path)
+#         await ragPipe.indexer.add_files2vdb(file_path)
+#     except Exception as e:
+#         raise HTTPException(status_code=500, detail=str(e))
+#     finally:
+#         pass
+#         # Delete the temporary file after processing
+#         # if os.path.exists(file.filename):
+#         #     os.remove(file.filename)
+
+
+# @app.post("/files_to_db/",
+#           summary="Add file(s) to the Quandrant VDB",
+#           tags=[Tags.VDB]
+#           )
+# async def upload_files(
+#     files: Annotated[
+#         list[UploadFile], 
+#         File(
+#             title="PDF Files", 
+#             description="PDF files to add to the vector database"
+#         )], 
+#     ):
+#     tasks = [process_data(file) for file in files]
+#     await asyncio.gather(*tasks)
---
+++ app_front.py
@@ -0,0 +1,189 @@
+from pathlib import Path
+from urllib.parse import urlparse, quote
+import chainlit as cl
+import yaml, torch
+from loguru import logger
+from io import BytesIO
+import json
+import httpx
+
+
+BASE_URL = "http://localhost:8082/{method}/"
+APP_DIR = Path.cwd().absolute()
+
+headers = {
+    "accept": "application/json", 
+    "Content-Type": "application/json"
+}
+
+history = []
+
+# https://github.com/Cinnamon/kotaemon/blob/main/libs/ktem/ktem/reasoning/prompt_optimization/suggest_followup_chat.py
+
+@cl.set_starters
+async def set_starters():
+    with open(APP_DIR / 'public' / 'conversation_starters.yaml') as file: # Load the YAML file
+        data = yaml.safe_load(file)
+        
+    return [
+        cl.Starter(
+            label=item["label"],
+            message=item["message"],
+            icon=item["icon"]
+        )
+        for item in data['starters']
+    ]
+     
+
+def format_elements(sources, only_txt=True):
+    elements = []
+    source_names = []
+    for doc in sources:
+        url = quote(doc['url'], safe=':/')
+        parsed_url = urlparse(doc['url'])
+        doc_name = parsed_url.path.split('/')[-1]
+        
+        if only_txt:
+            elem = cl.Text(content=doc["content"], name=doc['doc_id'], display='side')
+            
+        else:
+            source = Path(url)
+            match source.suffix:
+                case '.pdf':
+                    elem = cl.Pdf(name=doc['doc_id'], url=url, page=doc["page"], display='side')
+                case '.mp4':
+                    elem = cl.Video(name=doc['doc_id'], url=url, display='side')                
+                case '.mp3':
+                    elem = cl.Audio(name=doc['doc_id'], url=url, display='side')                
+                case _:
+                    elem = cl.Text(content=doc["content"], name=doc['doc_id'], display='side', url=url) # TODO Maybe HTML (convert the File first)
+
+        s = f"{doc['doc_id']}: {doc_name}"
+        elements.append(elem) 
+        source_names.append(s)               
+    return elements, source_names
+
+
+@cl.on_chat_start
+async def on_chat_start():
+    try:
+        global history
+        history.clear()
+        logger.info("New Chat Started")
+        async with httpx.AsyncClient(timeout=httpx.Timeout(60.0)) as client:
+            response = await client.get(url=BASE_URL.format(method='hello'))
+            print(response.text)
+    except:
+        logger.warning("Make sur the fastapi is up!!")
+
+
+@cl.set_chat_profiles
+async def chat_profiles():
+    with open(APP_DIR / 'public' / 'conversation_starters.yaml') as file: # Load the YAML file
+        data = yaml.safe_load(file)
+    
+    async with httpx.AsyncClient(timeout=httpx.Timeout(60.0)) as client:
+        response = await client.get(url=BASE_URL.format(method='collections'))
+        collections = json.loads(response.text)
+    
+    return [
+        cl.ChatProfile(
+            **profile,
+        ) 
+        for profile in data['chat_profiles'] if profile['name'] in collections
+    ]
+
+    
+@cl.on_message
+async def on_message(message: cl.Message):
+    global history
+    user_message = message.content
+    params = {
+            "new_user_input": user_message
+        }
+    async with cl.Step(name="Searching for relevant documents...") as step:
+        async with httpx.AsyncClient(timeout=httpx.Timeout(60.0)) as client:
+            async with client.stream(
+                'POST',
+                BASE_URL.format(method='generate'), 
+                params=params, 
+                headers=headers,
+                json=history
+            ) as streaming_response:
+                metadata_sources = streaming_response.headers.get("X-Metadata-Sources")
+                sources = json.loads(metadata_sources)
+
+                if sources:
+                    elements, source_names = format_elements(sources, only_txt=False)
+                    msg = cl.Message(content="", elements=elements)
+                else:
+                    msg = cl.Message(content="")
+                
+                await msg.send()
+                answer_txt = ""
+                async for token in streaming_response.aiter_bytes():
+                    token = token.decode()
+                    await msg.stream_token(token)
+                    answer_txt += token
+    
+    history.extend([
+        {'role': 'user', 'content': user_message},
+        {'role': 'user', 'content': answer_txt}
+    ])
+
+    if sources:
+        await msg.stream_token( '\n\n' + '-'*50 + "\n\nRetrieved Docs: \n" + '\n'.join(source_names))
+        
+    await msg.send()
+    torch.cuda.empty_cache()
+
+
+
+# from pydub import AudioSegment
+# import whisperx
+# from chainlit.element import ElementBased
+# device = 'cuda' if torch.cuda.is_available() else 'cpu'
+# audio_transcriber = AudioTranscriber(device=device).model
+
+# @cl.on_audio_chunk
+# async def on_audio_chunk(chunk: cl.AudioChunk):
+#  if chunk.isStart:
+#      buffer = BytesIO()
+#      buffer.name = f"input_audio.{chunk.mimeType.split('/')[1]}"
+#      cl.user_session.set("audio_buffer", buffer)
+#      cl.user_session.set("audio_mime_type", chunk.mimeType)
+
+#  # Write the chunks to a buffer
+#  cl.user_session.get("audio_buffer").write(chunk.data)
+
+# @cl.on_audio_end
+# async def on_audio_end(elements: list[ElementBased]):
+#  audio_buffer: BytesIO = cl.user_session.get("audio_buffer")
+#  # audio_mime_type: str = cl.user_session.get("audio_mime_type")
+#  audio_buffer.seek(0)  # Move the file pointer to the beginning
+
+#  try:
+#      sound = AudioSegment.from_file(audio_buffer)
+#      sound.export(
+#          "output.wav", format="wav", 
+#          parameters=["-ar", "16000", "-ac", "1", "-ab", "32k"]
+#      )
+#      trans_res= audio_transcriber.transcribe(
+#          audio=whisperx.load_audio('output.wav'), batch_size=8
+#      )
+#      transcription = ' '.join(s['text'] for s in trans_res["segments"])
+
+#      await cl.Message(content=f"transcription: {transcription}").send()
+
+#      await on_message(cl.Message(content=transcription))
+
+#  except Exception as e:
+#      await cl.Message(content=f"Error processing audio: {str(e)}").send()
+
+
+if __name__ == "__main__":
+    import sys
+    from chainlit.cli import run_chainlit
+    run_chainlit(__file__)
+
+# chainlit run app_front.py --host 0.0.0.0 --port 8001 --root-path /chainlit
