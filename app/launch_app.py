149a0e7bfedd26ee3372d2c64de1cfe21445d62d
Ahmath-GADJI <ahmathgadji27@gmail.com>
2025-02-25 10:37:45 +0000

    app folder deleted

--- app/launch_app.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from chainlit.utils import mount_chainlit
-from starlette.requests import Request
-from starlette.responses import RedirectResponse
-
-app = FastAPI()
-
-mount_chainlit(app=app, target="./chainlit_app.py", path="/chainlit")
-# uvicorn launch_app:app --host 0.0.0.0 --port 8000 --reload
