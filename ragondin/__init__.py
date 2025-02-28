e6741c6aad75cb00d4da91aff07fe4d5bfcd28f3
htagourti <htagourti@linagora.com>
2025-02-28 15:38:25 +0000

    restructure repo

---
+++ ragondin/__init__.py
@@ -0,0 +1,6 @@
+from components.pipeline import RagPipeline
+from ragondin.config import load_config
+from components.indexer import Indexer
+
+__all__ = [RagPipeline, load_config, Indexer]
+
--- src/__init__.py
+++
@@ -1,6 +0,0 @@
-from src.components.pipeline import RagPipeline
-from src.components.config import load_config
-from src.components.indexer import Indexer
-
-__all__ = [RagPipeline, load_config, Indexer]
-
