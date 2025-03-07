b965fb2ca25927e62d834573d1b1851757eada3a
EnjoyBacon7 <59032058+EnjoyBacon7@users.noreply.github.com>
2025-03-07 14:19:38 +0100

    Added marker
    - Add variable to toggle saving markdown file
    - Conditionalized Config override log
    - Added llm descriptions of images in marker

--- ragondin/config/config.py
+++ ragondin/config/config.py
@@ -6,7 +6,8 @@ from loguru import logger
 
 def load_config(config_path="../../.hydra_config", overrides=None)-> OmegaConf:
     load_dotenv()
-    # logger.debug(f"Config overrides: {overrides}")
+    if overrides:
+        logger.info(f"Config overrides: {overrides}")
         
         # TODO: I set the version base to 1.1 to silence the warning message, review how we want to handle versioning
     with initialize(config_path=config_path, job_name="config_loader", version_base='1.1'):
