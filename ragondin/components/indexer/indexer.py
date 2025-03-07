70ac2aeec0c94c658f40e074f057464da290bdc9
EnjoyBacon7 <59032058+EnjoyBacon7@users.noreply.github.com>
2025-03-07 14:19:38 +0100

    Added marker
    - Add variable to toggle saving markdown file
    - Conditionalized Config override log
    - Added llm descriptions of images in marker

--- ragondin/components/indexer/indexer.py
+++ ragondin/components/indexer/indexer.py
@@ -22,7 +22,7 @@ class Indexer:
     async def add_files2vdb(self, path: str | list[str]):
         """Add a files to the vector database in async mode"""
         try:
-            doc_generator: AsyncGenerator[Document, None] = self.serializer.serialize_documents(path, recursive=True)
+            doc_generator: AsyncGenerator[Document, None] = self.serializer.serialize_documents(path, recursive=True, save_md=True)
             await self.vectordb.async_add_documents(
                 doc_generator=doc_generator, 
                 chunker=self.chunker, 
