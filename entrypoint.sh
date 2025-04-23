1ea97ca272dcb5c496c746cd309651cfade84830
antncy <an2002lsth@gmail.com>
2025-04-23 10:54:06 +0000

    add env var for dashboard port

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,2 @@
-#!/bin/bash
-
-if [ -f .env ]; then
-    export $(grep -v '^#' .env | xargs)
-fi
-
-uv run ray start --head --dashboard-host 0.0.0.0
+uv run ray start --head --dashboard-host 0.0.0.0 -dashboard-port ${RAY_DASHBOARD_PORT:-8265}
 uv run --no-dev uvicorn api:app --host 0.0.0.0 --port ${CONTAINER_PORT:-8080} --reload
