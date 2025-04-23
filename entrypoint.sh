54b800ebba83bb6101687620db2f5df84ad0efcc
htagourti <htagourti@linagora.com>
2025-04-23 12:54:42 +0000

    fix typo

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
-uv run ray start --head --dashboard-host 0.0.0.0 -dashboard-port ${RAY_DASHBOARD_PORT:-8265}
+uv run ray start --head --dashboard-host 0.0.0.0 --dashboard-port ${RAY_DASHBOARD_PORT:-8265}
 ENV_ARG=""
 if [[ -n "${SHARED_ENV}" ]]; then
   ENV_ARG="--env-file=${SHARED_ENV}"
