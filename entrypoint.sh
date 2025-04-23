9900ee361aaff9189aa27dd53d10d065ef7d572c
antncy <an2002lsth@gmail.com>
2025-04-23 11:04:52 +0000

    Added uv .env file argument

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,6 @@
 uv run ray start --head --dashboard-host 0.0.0.0 -dashboard-port ${RAY_DASHBOARD_PORT:-8265}
-uv run --no-dev uvicorn api:app --host 0.0.0.0 --port ${CONTAINER_PORT:-8080} --reload
+ENV_ARG=""
+if [[ -n "${UV_ENV_FILE}" ]]; then
+  ENV_ARG="--env-file=${UV_ENV_FILE}"
+fi
+uv run --no-dev $ENV_ARG uvicorn api:app --host 0.0.0.0 --port ${CONTAINER_PORT:-8080} --reload
