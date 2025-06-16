74c0c95e64b3cc5fa02ee699ab630fdfd99d052d
Ahmath-Gadji <ahmathgadji27@gmail.com>
2025-06-16 09:15:14 +0000

    Refacto docker compose

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,12 +5,4 @@ if [[ -n "${SHARED_ENV}" ]]; then
   ENV_ARG="--env-file=${SHARED_ENV}"
 fi
 
-if [ "$PERSISTENCY" = "enabled" ]; then
-    echo "Initializing submodule for persistency..."
-    git submodule update --init --recursive
-    # Additional setup commands for the submodule
-else
-    echo "Starting services without persistence..."
-fi
-
 uv run --no-dev $ENV_ARG uvicorn api:app --host 0.0.0.0 --port ${APP_iPORT:-8080} --reload
