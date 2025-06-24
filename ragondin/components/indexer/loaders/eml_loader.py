4e12032087fef5ed039de78cbcc0b3285f6fdaad
antncy <an2002lsth@gmail.com>
2025-06-24 11:59:17 +0000

    Better exceptions in eml loader

--- ragondin/components/indexer/loaders/eml_loader.py
+++ ragondin/components/indexer/loaders/eml_loader.py
@@ -14,19 +14,15 @@ class EmlLoader(BaseLoader):
         super().__init__(**kwargs)
 
     async def aload_document(self, file_path, metadata: dict = None, save_markdown: bool = False):
-        path = Path(file_path)
-        path = path.expanduser().resolve()
+        try:
+            with open(file_path, 'rb') as fhdl:
+                raw_email = fhdl.read()
 
-        with open(path, 'rb') as fhdl:
-            raw_email = fhdl.read()
+            ep = eml_parser.EmlParser(include_raw_body=True)
+            parsed_eml = ep.decode_email_bytes(raw_email)
+            content_body = parsed_eml['body'][0]['content']
+        except Exception as e:
+            raise ValueError(f"Failed to parse the EML file {file_path}: {e}")
 
-        ep = eml_parser.EmlParser(include_raw_body=True)
-        parsed_eml = ep.decode_email_bytes(raw_email)
-        content_body = parsed_eml['body'][0]['content']
-
-        document = Document(page_content=content_body, metadata=metadata)
-
-        if not document:
-            raise ValueError(f"The file {file_path} could not be converted to Markdown.")
-        
+        document = Document(page_content=content_body, metadata=metadata)        
         return document
