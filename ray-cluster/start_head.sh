8b4aaf66f00e6248aae7ab9297ed81886043484e
htagourti <htagourti@linagora.com>
2025-07-02 12:15:59 +0000

    custom head node ip

--- ray-cluster/start_head.sh
+++ ray-cluster/start_head.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-uv run ray start --head --dashboard-host 0.0.0.0 --dashboard-port ${RAY_DASHBOARD_PORT:-8265}
+uv run ray start --head --dashboard-host 0.0.0.0 --dashboard-port ${RAY_DASHBOARD_PORT:-8265} --node-ip-address ${HEAD_NODE_IP}
 tail -f /dev/null
